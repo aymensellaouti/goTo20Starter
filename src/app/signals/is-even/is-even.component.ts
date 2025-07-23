4a142d918eb2b822b09ca9fea33c86286ef00764
aymensellaouti <aymen.sellaouti@gmail.com>
2025-07-23 16:56:10 +0100

    isEven input starter

---
+++ src/app/signals/is-even/is-even.component.ts
@@ -0,0 +1,11 @@
+import { Component, computed, input, Input, OnChanges, output, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-is-even',
+  imports: [],
+  templateUrl: './is-even.component.html',
+  styleUrl: './is-even.component.css',
+})
+export class IsEvenComponent {
+  x = 0;
+}
