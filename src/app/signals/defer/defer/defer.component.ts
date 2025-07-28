5be3cb3a5e43e3f59585c933d3f891ee834eff9b
aymensellaouti <aymen.sellaouti@gmail.com>
2025-07-28 09:15:21 +0100

    starter defer example

---
+++ src/app/signals/defer/defer/defer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HugeComponent } from "../huge/huge.component";
+
+@Component({
+    selector: 'app-defer',
+    templateUrl: './defer.component.html',
+    styleUrl: './defer.component.css',
+    imports: [HugeComponent]
+})
+export class DeferComponent {
+
+}
