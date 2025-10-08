f5542b29f1349fde4590aa79c59c1fc50d1ad1e3
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-08 10:05:24 +0100

    exemple currencySelector

--- src/app/signals/linkedSignal/currency-selector/country-currency-selector.component.ts
+++ src/app/signals/linkedSignal/currency-selector/country-currency-selector.component.ts
@@ -1,7 +1,8 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, linkedSignal, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { countries } from '../data/countries';
 import { JsonPipe } from '@angular/common';
+import { Country } from '../model/country.interface';
 
 @Component({
   selector: 'app-country-currency-selector',
@@ -12,5 +13,23 @@ import { JsonPipe } from '@angular/common';
 export class CountryCurrencySelectorComponent {
   countries = signal(countries);
   selectedCountry = signal(countries[0]);
-  selectedCurrency = computed(() => this.selectedCountry().acceptedCurrencies[0]);
+  /**
+   * On va dépendre de selectedCountry => a chaque modification du pays sélectionné voila ce que je fais :
+   *
+   * Si l'ancienne valeur de devise est incluse dans la liste des monnies du nouveau pays sélectionné
+   * Je la garde
+   * Sinon on mette celle par défaut du nouveau pays séléctionné
+   */
+  selectedCurrency = linkedSignal<Country, string>({
+    // LEs signaux dont je dépend
+    source: this.selectedCountry,
+    // Comment je calcule la nouvelle valeur
+    computation: (newCountry, previousValues) => {
+      const oldSelectedCurrency = previousValues?.value ?? '';
+      return newCountry.acceptedCurrencies.includes(oldSelectedCurrency)
+            ? oldSelectedCurrency
+            : newCountry.defaultCurrency
+    }
+  })
+
 }
